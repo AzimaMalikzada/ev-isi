5ac6eb64815e3e195d449e5deceeea71125f0781
azima8888 <110724271+azima8888@users.noreply.github.com>
2022-10-01 13:04:57 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict'
+var ad = "1.Mənim adım Əzimədir."
+document.getElementsByClassName('demo')[0].innerHTML = "1.Menim adim azimadir."
+console.log(ad)
+
+
+var soyad = "2.Soyadım isə Məlikzadədir."
+document.getElementsByClassName('demo')[1].innerHTML = "2.Soyadım isə Məlikzadədir."
+console.log(soyad)
+
+   
+var men = "3.Men"
+var tevellüd = + z
+var ucar = " təvəllüdündə Ucarda dünyaya gəlmişəm. "
+var x = 2000
+var y = 4
+var z = x + y
+document.getElementsByClassName('demo')[2].innerHTML = "3.Mən " + z + " təvəllüdündə Ucarda dünyaya gəlmişəm. "
+console.log(men +  z + " təvəllüdündə Ucarda dünyaya gəlmişəm.")
+
+
+var reng = "4.Ən sevdiyim rəng qırmızıdır."
+document.getElementsByClassName('demo')[3].innerHTML = "4.Ən sevdiyim rəng qırmızıdır."
+console.log(reng)
+
+
+var film = "5.Ən sevdiyim film Savaş Vadisidir."
+document.getElementsByClassName('demo')[4].innerHTML = "5.Ən sevdiyim film Savaş Vadisidir."
+console.log(film)
+
+
+var hal = "6.Hal-hazırda"
+var cariİl = 2022
+var doğulduğumİl = 2004
+var yaş = 2022 - 2004
+document.getElementsByClassName('demo')[5].innerHTML = "5.Hal-hazırda " + yaş  + " yaşım var."
+console.log("6.Hal-hazırda " + yaş  + " yaşım var.")
+
+var web = "7.Mən gələcəkdə İnşallah tanınan 'Web-Proqramçı olacam."
+document.getElementsByClassName('demo')[6].innerHTML = "7.Mən gələcəkdə İnşallah tanınan 'Web-Proqramçı' olacam."
+console.log(web)
